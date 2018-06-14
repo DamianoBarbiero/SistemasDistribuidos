7edfb9a2b36ea898c8a521e44821ed4462a582d9
DamianoBarbiero <damiano_junior@hotmail.com>
2018-06-14 08:36:33 -0300

    .

---
+++ ProjAngular/src/teste/Teste.java
@@ -0,0 +1,26 @@
+package teste;
+
+import controle.CarroCtrl;
+import entidade.Carro;
+
+public class Teste {
+	
+	public static void main (String[] args) {
+		
+		Carro c = new Carro();
+		
+		c.setModelo("FORD");
+		c.setNome("KA");
+		
+		CarroCtrl controle = new CarroCtrl();
+		controle.insert(c);
+		
+		for(Carro carro : new CarroCtrl().select(Carro.CONS_CARRO_ALL)) {
+			
+			System.out.println("Id: " + carro.getId());
+			System.out.println("Nome: " + carro.getNome());
+			System.out.println("Modelo: " + carro.getModelo());
+		}
+	}
+
+}
